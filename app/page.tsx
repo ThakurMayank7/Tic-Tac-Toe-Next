957c76f34cd86e3d6aa3d15779241b515db943f8
Mayank <mayanksingh8181335335@gmail.com>
2024-11-17 23:49:13 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
     
     
         <Grid/>
+        <br />
         <Footer/>
     </>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className="container mx-auto text-center">
+            <p className="text-sm">Created by <strong>Mayank Singh</strong></p>
+            <div className="flex justify-center space-x-4 mt-2">
+              {/* <!-- LinkedIn Link with Icon --> */}
+              <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" className="text-blue-400 hover:text-blue-600">
+                <i className="fab fa-linkedin"></i> LinkedIn
+              </a>
+              {/* <!-- GitHub Link with Icon --> */}
+              <a href="https://github.com/thakurmayank5" target="_blank" className="text-gray-400 hover:text-gray-600">
+                <i className="fab fa-github"></i> GitHub
+              </a>
+            </div>
+          </div>
+    </footer>
   )
 }
 
