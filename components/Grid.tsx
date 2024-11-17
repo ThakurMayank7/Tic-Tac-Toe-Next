c34187e072dc6e9e7273fa0e38158bef5520d123
Mayank <mayanksingh8181335335@gmail.com>
2024-11-18 00:02:33 +0530

    added darkmode state to gridblock component

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -9,15 +9,15 @@ function Grid({darkmode}:GridProps) {
   return (
     <div id="Banner" className="flex justify-center items-center p-2">
             <div id="board" className="grid grid-cols-3 aspect-square h-96 gap-2">
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
-                <GridBlock/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
+                <GridBlock darkM={darkmode}/>
             </div>
         </div>
   )
--- components/ui/GridBlock.tsx
+++ components/ui/GridBlock.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-function GridBlock() {
+type GridBlockProps = {
+    darkM: boolean;
+  };
+
+function GridBlock({darkM}:GridBlockProps) {
   return (
     <div className="bg-teal-300 rounded aspect-square flex items-center justify-center max-h-32 hover:bg-teal-800 text-8xl" ></div>
   )
